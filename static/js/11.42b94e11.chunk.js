c0189eb2235a92fa1a511e95bebca8459d03736a
Vahid Goodarzi <Vahid031@yahoo.com>
2021-12-29 18:37:52 +0330

    Updates

---
+++ static/js/11.42b94e11.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[11],{836:function(e,t,r){"use strict";r.r(t);r(1);var c=r(21);t.default=function(){return Object(c.jsx)(c.Fragment,{})}}}]);
+//# sourceMappingURL=11.42b94e11.chunk.js.map
